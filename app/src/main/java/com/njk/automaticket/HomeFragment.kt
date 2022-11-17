6ec247027cd37541f7e263b9b509b9d01174cedc
nijuyonkadesu <24ksriramv@gmail.com>
2022-11-18 00:51:50 +0530

    add viewmodel

--- app/src/main/java/com/njk/automaticket/HomeFragment.kt
+++ app/src/main/java/com/njk/automaticket/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.installations.FirebaseInstallations
@@ -20,6 +21,7 @@ import com.njk.automaticket.model.TicketStatus
 import com.njk.automaticket.model.User
 import com.njk.moveit.viewmodels.TAG
 import com.njk.moveit.viewmodels.URL
+import com.njk.moveit.viewmodels.UserViewModel
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -31,68 +33,26 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-//    val sharedViewModel: UserViewModel by activityViewModels()
+    private val sharedViewModel: UserViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val database = Firebase.database(URL).getReference("Users")
-
-        Toast.makeText(requireContext(), "Home", Toast.LENGTH_SHORT).show()
 
         binding.testBtn.setOnClickListener {
-            FirebaseInstallations.getInstance().id.addOnCompleteListener(
-                OnCompleteListener { task ->
-                    if(!task.isSuccessful) {
-                        Log.w(TAG, "Fetching Unique ID failed", task.exception)
-                        return@OnCompleteListener
-                    }
-                    context?.getSharedPreferences("_", MODE_PRIVATE)?.edit()?.putString("id", task.result)?.apply()
-                    Log.d("firebase", "new unique Token: ${task.result}")
-                })
-            // TODO: Implement the same in a separate class by extending
-            FirebaseMessaging.getInstance().token.addOnCompleteListener(
-                OnCompleteListener { task ->
-                    if (!task.isSuccessful) {
-                        Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-                        return@OnCompleteListener
-                    }
-
-                    // Get new FCM registration token
-                    val fcm = FcmToken(task.result)
-                    val user = createUser(fcm)
-                    val id = context?.getSharedPreferences("_", MODE_PRIVATE)?.getString("id", "fail")!!
-
-                    database.child(id).setValue(user)
-                    context?.getSharedPreferences("_", FirebaseMessagingService.MODE_PRIVATE)?.edit()?.putString("fcm", task.result)?.apply()
-                    Log.d("firebase", "new FCM token: ${task.result}")
-                })
+            sharedViewModel.createNewFirebaseUser(requireContext())
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun createUser(fcm: FcmToken): User {
-        return User(
-            8787, // TODO: Get rfid from BarcodeScanning Activity
-            1000,
-            0, // TODO: Calculate from distance, accumulate, subtract, reset
-            TicketStatus.INVALID,
-            fcm,
-            0, // 0 because, this function is ran just once when user installs the app
-            0,
-        )
     }
 }
--- app/src/main/java/com/njk/automaticket/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/njk/automaticket/viewmodels/UserViewModel.kt
@@ -10,6 +10,7 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.njk.automaticket.model.FcmToken
+import com.njk.automaticket.model.TicketStatus
 import com.njk.automaticket.model.User
 
 
@@ -18,4 +19,53 @@ const val TAG = "firebase"
 
 class UserViewModel: ViewModel() {
 
+    // Reference to firebase database
+    private val database = Firebase.database(URL).getReference("Users")
+
+    fun getUserId(context: Context) {
+        FirebaseInstallations.getInstance().id.addOnCompleteListener(
+            OnCompleteListener { task ->
+                if(!task.isSuccessful) {
+                    Log.w(TAG, "Fetching Unique ID failed", task.exception)
+                    return@OnCompleteListener
+                }
+                context.getSharedPreferences("_", Context.MODE_PRIVATE)?.edit()?.putString("id", task.result)?.apply()
+                Log.d("firebase", "new unique Token: ${task.result}")
+            })
+    }
+    fun getFcmToken(context: Context){
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(
+            OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new FCM registration token
+                val fcm = FcmToken(task.result)
+                val user = createUser(fcm)
+                val id = context?.getSharedPreferences("_", Context.MODE_PRIVATE)?.getString("id", "fail")!!
+
+                database.child(id).setValue(user)
+                context?.getSharedPreferences("_", FirebaseMessagingService.MODE_PRIVATE)?.edit()?.putString("fcm", task.result)?.apply()
+                Log.d("firebase", "new FCM token: ${task.result}")
+            })
+    }
+    private fun createUser(fcm: FcmToken): User {
+        return User(
+            8787, // TODO: Get rfid from BarcodeScanning Activity
+            1000,
+            0, // TODO: Calculate from distance, accumulate, subtract, reset
+            TicketStatus.INVALID,
+            fcm,
+            0, // 0 because, this function is ran just once when user installs the app
+            0,
+        )
+    }
+
+    // Creates a new node in firebase realtime database
+    fun createNewFirebaseUser(context: Context){
+        getUserId(context)
+        getFcmToken(context)
+    }
 }
